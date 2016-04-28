54abb9a75c70471b7eebc594dbb46ab21ca2a85f
oxanayoxana <oksana.oleshchenko@gmail.com>
2016-04-28 15:14:15 +0000

    Initial Commit

--- js/scriptjs.js
+++ js/scriptjs.js
@@ -1,29 +1,8 @@
-var a,b,c,x,x1,x2;
-alert ('Lets do the math like >> ax2 + bx + c = 0, OK?');
+var ar=[];
+ar[0]=11;
+ar[1]=01;
+ar[2]=1990;
 
-while ( !a  ) a =Number(prompt( "First, enter any numeric value for a >> ax2 + bx + c = 0, where a≠0" ));
-if (a==0) {
-    a=!a;
-}
-while ( ! b ) b =Number(prompt( 'Now enter value for "b" >> ax2 + bx + c = 0' ));
-while ( ! c ) c =Number(prompt( 'And the last one "c" >> ax2 + bx + c = 0'));
-
-alert('Cool! Now we have:'+ a +'x2+ '+ b+ 'x +'+ c +'= 0, where a≠0, right?');
-
-var D=b*b-4*a*c;
-
-if (D>0) {
-    x1=(-b+Math.sqrt(D))/2*a;
-    x2=(-b-Math.sqrt(D))/2*a;
-    alert('x1='+x1+'; x2='+x2);
-}
-if (D==0) {
-    x=(b/2*a)*-1;
-    alert('x='+x);
-}
-if (D<0) {
-    x1=(-b+(-1)*Math.sqrt(D))/2*a;
-    x2=(-b-(-1)*Math.sqrt(D))/2*a;
-    alert('x1='+x1+'; x2='+x2);
-}
-    
+var sum;
+sum=ar[0]+ar[1]+ar[2];
+alert(sum);
